6e62e7a21afdd0f76fdc063eff84c6e6ae497aeb
Bram Boer <79155369+bram-boer@users.noreply.github.com>
2023-08-03 11:44:05 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,23 +1,55 @@
-// create webserver
-// create a route for POST /comments
-// use body-parser to parse the body of the request
-// send back a response with a JSON payload
-// the payload should be the same as the request body
-// start the server and test with Postman
-
-const express = require("express");
-const bodyParser = require("body-parser");
+// create web server with express
+const express = require('express');
 const app = express();
-
-const PORT = 3000;
-
+// create body parser
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-app.post('/comments', (req, res) => {
-  const { body } = req;
-  res.json(body);
+// create mongoose
+const mongoose = require('mongoose');
+// connect to mongoDB
+mongoose.connect('mongodb://localhost:27017/itcast', { useNewUrlParser: true, useUnifiedTopology: true });
+// create schema
+const Schema = mongoose.Schema;
+const commentSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: false
+    }
 });
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// create model
+const Comment = mongoose.model('Comment', commentSchema);
+// set view engine
+app.set('view engine', 'ejs');
+// set view path
+app.set('views', './views');
+// set static path
+app.use('/public', express.static('./public'));
+// create router
+app.get('/', (req, res) => {
+    Comment.find((err, data) => {
+        if (err) {
+            return res.status(500).send('Server error.');
+        }
+        res.render('index.html', { comments: data });
+    });
+});
+app.get('/post', (req, res) => {
+    res.render('post.html');
+});
+app.post('/post', (req, res) => {
+    const comment = new Comment(req.body);
+    comment.save((err, data) => {
+        if (err) {
+            return res.status(500).send('Server error.');
+        }
+        res.redirect('/');
+    });
+});
+app.listen(3000, () => {
+    console.log('Server is running at port 3000.');
 });
