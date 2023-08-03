8bd1a5833eee4f4032bbe89c3186a88cf1fc353e
Bram Boer <79155369+bram-boer@users.noreply.github.com>
2023-08-03 11:47:03 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,55 +1,87 @@
-// create web server with express
-const express = require('express');
-const app = express();
-// create body parser
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-// create mongoose
-const mongoose = require('mongoose');
-// connect to mongoDB
-mongoose.connect('mongodb://localhost:27017/itcast', { useNewUrlParser: true, useUnifiedTopology: true });
+// create web server
+// create database connection
 // create schema
-const Schema = mongoose.Schema;
-const commentSchema = new Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    comment: {
-        type: String,
-        required: false
-    }
+// create model
+// create route
+// create view
+// create controller
+// create test
+
+// import modules
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const Comment = require('./models/comment');
+const CommentController = require('./controllers/commentController');
+
+// create web server
+const app = express();
+
+// create database connection
+mongoose.connect('mongodb://localhost:27017/comments', { useNewUrlParser: true });
+
+// create schema
+const commentSchema = new mongoose.Schema({
+  name: String,
+  comment: String
 });
+
 // create model
 const Comment = mongoose.model('Comment', commentSchema);
+
 // set view engine
 app.set('view engine', 'ejs');
-// set view path
-app.set('views', './views');
-// set static path
-app.use('/public', express.static('./public'));
-// create router
+
+// set public directory
+app.use(express.static('public'));
+
+// set body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// create route
 app.get('/', (req, res) => {
-    Comment.find((err, data) => {
-        if (err) {
-            return res.status(500).send('Server error.');
-        }
-        res.render('index.html', { comments: data });
+  Comment.find({}, (err, comments) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('index', { comments: comments });
+    }
+  });
+});
+
+app.post('/', (req, res) => {
+  const comment = new Comment(req.body);
+  comment.save(err => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect('/');
+    }
+  });
+});
+
+// create controller
+const CommentController = require('./controllers/commentController');
+
+// create test
+describe('CommentController', () => {
+  describe('getComments', () => {
+    it('should return all comments', done => {
+      const Comment = {
+        find: sinon.spy()
+      };
+
+      const commentController = CommentController(Comment);
+
+      commentController.getComments((err, comments) => {
+        Comment.find.calledOnce.should.be.true;
+        done();
+      });
     });
+  });
 });
-app.get('/post', (req, res) => {
-    res.render('post.html');
-});
-app.post('/post', (req, res) => {
-    const comment = new Comment(req.body);
-    comment.save((err, data) => {
-        if (err) {
-            return res.status(500).send('Server error.');
-        }
-        res.redirect('/');
-    });
-});
+
+// listen on port
 app.listen(3000, () => {
-    console.log('Server is running at port 3000.');
+  console.log('server listening on port 3000');
 });
